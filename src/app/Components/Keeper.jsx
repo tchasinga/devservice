77d8651e22b4b1305283bc8677cbe0a3a016d9e1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-12 17:41:46 +0200

    new code is added now

--- src/app/Components/Keeper.jsx
+++ src/app/Components/Keeper.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AnimatedGridPattern from "../Animation/magicui/animated-grid-pattern";
 import { cn } from "../../lib/utils.ts";
+import Navbars from "./Ui/Navbars";
 
 export default function Keeper() {
     return (
         <div className="relative  min-h-screen w-full  overflow-hidden ">
+            <Navbars/>
           <AnimatedGridPattern
             numSquares={230}
             maxOpacity={0.2}
@@ -12,7 +14,7 @@ export default function Keeper() {
             repeatDelay={1}
             className={cn(
               "[mask-image:radial-gradient(1500px_circle_at_center,white,transparent)]",
-              "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+              "inset-x-0 inset-y-[-30%] h-[200%]",
             )}
           />
         </div>
