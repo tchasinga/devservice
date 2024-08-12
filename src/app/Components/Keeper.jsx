a26ab792b719063b06bbef1e76c60519901e0c41
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-12 17:31:49 +0200

    new changes is added now

--- src/app/Components/Keeper.jsx
+++ src/app/Components/Keeper.jsx
@@ -4,17 +4,14 @@ import { cn } from "../../lib/utils.ts";
 
 export default function Keeper() {
     return (
-        <div className="relative flex min-h-screen w-full items-center justify-center overflow-hidden  bg-background p-20 md:shadow-xl">
-          <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
-            Animated Grid Pattern
-          </p>
+        <div className="relative  min-h-screen w-full  overflow-hidden ">
           <AnimatedGridPattern
-            numSquares={30}
-            maxOpacity={0.1}
+            numSquares={230}
+            maxOpacity={0.2}
             duration={3}
             repeatDelay={1}
             className={cn(
-              "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+              "[mask-image:radial-gradient(1500px_circle_at_center,white,transparent)]",
               "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
             )}
           />
