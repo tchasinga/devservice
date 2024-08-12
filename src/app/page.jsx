a20201e21abc2faf4a3462a4b1a655d3183b479f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-12 17:09:35 +0200

    new changes is adde now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,9 @@
 import Image from "next/image";
-import WordRotate from "./Animation/magicui/wordrotate";
 
-
-const words = ["1", "2", "good", "mailing"]
 
 export default function Home() {
   return (
-    <div>
-       <h1 className="text-2xl text-red-950">
-        <WordRotate words={words} />
-       </h1>
-    </div>
+    <>
+    </>
   );
 }
