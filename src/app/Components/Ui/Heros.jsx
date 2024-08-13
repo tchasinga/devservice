77421d8d97f872ea5ee43b6d4456296af8953958
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-13 08:08:27 +0200

    new desing is now

--- src/app/Components/Ui/Heros.jsx
+++ src/app/Components/Ui/Heros.jsx
@@ -5,7 +5,12 @@ export default function Heros() {
     <div className='absolute top-[50%]'>
         {/* new first div */}
         <div className="flex flex-col justify-center">
-            <p className="text-blue-900">we can provide more</p>
+            <p className="text-blue-950">welcom to dev-service</p>
+            <div className="flex flex-col justify-center text-3xl text-slate-900 font-extrabold">
+                <h1>We solve business</h1>
+                <h1>problems with</h1>
+                <h1>technology</h1>
+            </div>
         </div>
 
         {/* Second div */}
