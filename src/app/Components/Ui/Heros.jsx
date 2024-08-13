2022d8ee87171d80d6236e52b490e14445f7bcd0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-13 10:17:52 +0200

    new balolebwami is added now

--- src/app/Components/Ui/Heros.jsx
+++ src/app/Components/Ui/Heros.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import myImgIsadded from '../Images/REdI.png'
 import Image from 'next/image'
@@ -6,40 +5,38 @@ import "./extension.css";
 
 export default function Heros() {
   return (
-    <div className='absolute top-[20%] w-full onrespo'>
-       <div className="flex justify-between items-center max-w-screen-xl mx-auto border-black border onrespo">
-               {/* new first div */}
-        <div className="flex flex-col justify-center">
-            <p className="text-blue-950 font-semibold">welcom to dev-service</p>
-            <div className="flex flex-col justify-center text-5xl text-slate-900 font-extrabold mt-2">
-                <h1>We solve business</h1>
-                <h1>problems with</h1>
-                <h1>technology</h1>
-            </div>
-            <div className="my-5 text-slate-900 font-normal">
-                <p>Our performance is your success, our passion is</p>
-                <p>Innovation, our expertise is unmatched, we get you</p>
-                <p>More</p>
-            </div>
-            <div className="flex items-center w-full">
-                <div className="bg-blue-400 p-4 cursor-pointer duration-500 flex justify-center items-center border w-1/2">
-                    <p>Get Started</p>
-                </div>
-    
-                <div className="bg-blue-400 p-4 cursor-pointer duration-500 flex justify-center items-center border w-1/2 ">
-                    <p>Get Started</p>
-                </div>
-            </div>
-        </div>
+    <div className='relative w-full py-[200px] px-4 md:px-8'>
+       <div className="flex flex-col md:flex-row justify-between items-center max-w-screen-xl mx-auto border-black border onrespo">
+          {/* New first div */}
+          <div className="flex flex-col justify-center text-center md:text-left">
+              <p className="text-blue-950 font-semibold text-lg md:text-xl">Welcome to Dev-Service</p>
+              <div className="flex flex-col justify-center text-4xl md:text-5xl text-slate-900 font-extrabold mt-2">
+                  <h1>We solve business</h1>
+                  <h1>problems with</h1>
+                  <h1>technology</h1>
+              </div>
+              <div className="my-5 text-slate-900 font-normal text-sm md:text-base">
+                  <p>Our performance is your success, our passion is</p>
+                  <p>Innovation, our expertise is unmatched, we get you</p>
+                  <p>More</p>
+              </div>
+              <div className="flex flex-col md:flex-row items-center w-full gap-4">
+                  <div className="bg-blue-400 p-4 cursor-pointer duration-500 flex justify-center items-center border w-full md:w-1/2">
+                      <p>Get Started</p>
+                  </div>
+                  <div className="bg-blue-400 p-4 cursor-pointer duration-500 flex justify-center items-center border w-full md:w-1/2">
+                      <p>Get Started</p>
+                  </div>
+              </div>
+          </div>
 
-       {/* Second div is here */}
+         {/* Second div is here */}
         <div className="flex  justify-center items-center">
             <div className='w-full relative'>
             <div className="w-[500px] h-[500px] top-[-3%] right-0 left-0  bottom-0 absolute z-[-9999] rounded-full bg-green-700"></div>
                  <Image src={myImgIsadded}/>
             </div>
         </div>
-
        </div>
     </div>
   )
