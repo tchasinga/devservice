6f55be049ffe839d46bac329bcdfedfb95bcef50
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-13 10:40:04 +0200

    new code is added now

--- src/app/Components/Ui/Heros.jsx
+++ src/app/Components/Ui/Heros.jsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import myImgIsadded from '../Images/REdI.png'
-import Image from 'next/image'
+import React from "react";
+import myImgIsadded from "../Images/REdI.png";
+import Image from "next/image";
 import "./extension.css";
+import ShimmerButton from "../../../app/Animation/magicui/ShimmerButtonDemo";
 
 export default function Heros() {
   return (
-    <div className='relative w-full py-[200px] px-4 md:px-8'>
-       <div className="flex flex-col md:flex-row justify-between items-center max-w-screen-xl mx-auto onrespo">
-          {/* New first div */}
-          <div className="flex flex-col justify-center text-center md:text-left">
-              <p className="text-blue-950 font-semibold text-lg md:text-xl">Welcome to Dev-Service</p>
-              <div className="flex flex-col justify-center text-4xl md:text-5xl text-slate-900 font-extrabold mt-2 first-letter:uppercase">
-                  <h1>We solve business</h1>
-                  <h1>Problems with</h1>
-                  <h1>Technology</h1>
-              </div>
-              <div className="my-5 text-slate-900 font-normal text-sm md:text-base">
-                  <p>Our performance is your success, our passion is</p>
-                  <p>Innovation, our expertise is unmatched, we get you</p>
-                  <p>More</p>
-              </div>
-              <div className="flex flex-col md:flex-row items-center w-full gap-4">
-                  <div className="bg-blue-400 p-4 cursor-pointer duration-500 flex justify-center items-center border w-full md:w-1/2">
-                      <p>Get Started</p>
-                  </div>
-                  <div className="bg-blue-400 p-4 cursor-pointer duration-500 flex justify-center items-center border w-full md:w-1/2">
-                      <p>Get Started</p>
-                  </div>
-              </div>
+    <div className="relative w-full py-[200px] px-4 md:px-8">
+      <div className="flex flex-col md:flex-row justify-between items-center max-w-screen-xl mx-auto onrespo">
+        {/* New first div */}
+        <div className="flex flex-col justify-center text-center md:text-left">
+          <p className="text-blue-950 font-semibold text-lg md:text-xl">
+            Welcome to Dev-Service
+          </p>
+          <div className="flex flex-col justify-center text-4xl md:text-5xl text-slate-900 font-extrabold mt-2 first-letter:uppercase">
+            <h1>We solve business</h1>
+            <h1>Problems with</h1>
+            <h1>Technology</h1>
           </div>
-
-         {/* Second div is here */}
-        <div className="flex  justify-center items-center">
-            <div className='w-full relative'>
-            <div className="w-[500px] h-[500px] top-[-3%] right-0 left-0  bottom-0 absolute z-[-9999] rounded-full bg-green-700"></div>
-                 <Image src={myImgIsadded}/>
+          <div className="my-5 text-slate-900 font-normal text-sm md:text-base">
+            <p>Our performance is your success, our passion is</p>
+            <p>Innovation, our expertise is unmatched, we get you</p>
+            <p>More</p>
+          </div>
+          <div className="flex flex-col md:flex-row items-center w-full gap-4">
+           
+              <ShimmerButton className="shadow-2xl">
+              <div className="bg-slate-900 text-slate-300 p-4 cursor-pointer duration-500 flex justify-center items-center border w-full md:w-1/2">
+                 <p>View services</p>
             </div>
+              </ShimmerButton>
+
+            <div className="bg-slate-900 text-slate-300 p-4 cursor-pointer duration-500 flex justify-center items-center border w-full md:w-1/2">
+              <p>View services</p>
+            </div>
+          </div>
         </div>
-       </div>
+
+        {/* Second div is here */}
+        <div className="flex  justify-center items-center">
+          <div className="w-full relative">
+            <div className="w-[500px] h-[500px] top-[-3%] right-0 left-0  bottom-0 absolute z-[-9999] rounded-full bg-green-700"></div>
+            <Image src={myImgIsadded} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
+      },
+      keyframes: {
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
       },
     },
   },
