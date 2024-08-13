880b97df1e519737f1719786b3ff7a2b872640b7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-13 08:10:45 +0200

    new code is added

--- src/app/Components/Ui/Heros.jsx
+++ src/app/Components/Ui/Heros.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 export default function Heros() {
   return (
-    <div className='absolute top-[50%]'>
+    <div className='absolute top-[10%]'>
         {/* new first div */}
         <div className="flex flex-col justify-center">
-            <p className="text-blue-950">welcom to dev-service</p>
-            <div className="flex flex-col justify-center text-3xl text-slate-900 font-extrabold">
+            <p className="text-blue-950 font-semibold">welcom to dev-service</p>
+            <div className="flex flex-col justify-center text-5xl text-slate-900 font-extrabold">
                 <h1>We solve business</h1>
                 <h1>problems with</h1>
                 <h1>technology</h1>
+            </div>
+            <div className="">
+                <p></p>
+                <p></p>
+                <p></p>
             </div>
         </div>
 
