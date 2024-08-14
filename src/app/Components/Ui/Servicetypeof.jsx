d99883c335a533e871bf8b64ef01ee193d0de11f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-14 15:00:49 +0200

    new code is added nown

--- src/app/Components/Ui/Servicetypeof.jsx
+++ src/app/Components/Ui/Servicetypeof.jsx
@@ -1,16 +1,20 @@
 "use client";
 import React from "react";
+import AnimatedTooltipPreview from "../Ui/Tooltips";
+
 
 export default function Servicetypeof() {
   return (
-    <div className="min-h-screen max-w-screen-xl mx-auto w-full">
+    <div className="min-h-screen max-w-screen-xl mx-auto w-full overflow-hidden">
       <div className="flex flex-col justify-center pt-[100px]">
         <h1 className="text-slate-900 text-2xl font-bold">
           We provide solution on your bussness
+          <hr className="mt-1 py-2 w-[150px]"/>
         </h1>
         
-        {/* Right index comments */}
-        <div className="flex flex-col justify-center">
+        <div className="flex items-center justify-around flex-wrap w-full">
+            {/* Right index comments */}
+        <div className="flex flex-col justify-center text-slate-900 font-medium">
           <p>
             Our mission is to leverage cutting-edge technology to bring your
             vision to life.
@@ -22,7 +26,12 @@ export default function Servicetypeof() {
         </div>
 
         {/* Left index comments */}
+        <div className="">
+            <AnimatedTooltipPreview />
+        </div>
+        </div>
 
+        {/* Displaying data  */}
       </div>
     </div>
   );
