344a6e987f74022bd15252b2eded145c3b7f300c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-14 12:00:55 +0200

    new commit is added now

--- src/app/Components/Ui/Servicetypeof.jsx
+++ src/app/Components/Ui/Servicetypeof.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 
 export default function Servicetypeof() {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen max-w-screen-xl mx-auto w-full'>
+        <div className="flex flex-col justify-center pt-[100px]">
+            <h1 className='text-slate-900 text-2xl font-bold'>We provide solution on your bussness</h1>
+            <div className="flex flex-col justify-center">
+                <p></p>
+                <p></p>
+            </div>
+        </div>
     </div>
   )
 }
