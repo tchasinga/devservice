8122ec52f139a2dcf568f747e228007454df1d61
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-14 12:14:03 +0200

    new changes is added for really

--- src/app/Components/Ui/Servicetypeof.jsx
+++ src/app/Components/Ui/Servicetypeof.jsx
@@ -1,16 +1,29 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
 export default function Servicetypeof() {
   return (
-    <div className='min-h-screen max-w-screen-xl mx-auto w-full'>
-        <div className="flex flex-col justify-center pt-[100px]">
-            <h1 className='text-slate-900 text-2xl font-bold'>We provide solution on your bussness</h1>
-            <div className="flex flex-col justify-center">
-                <p></p>
-                <p></p>
-            </div>
+    <div className="min-h-screen max-w-screen-xl mx-auto w-full">
+      <div className="flex flex-col justify-center pt-[100px]">
+        <h1 className="text-slate-900 text-2xl font-bold">
+          We provide solution on your bussness
+        </h1>
+        
+        {/* Right index comments */}
+        <div className="flex flex-col justify-center">
+          <p>
+            Our mission is to leverage cutting-edge technology to bring your
+            vision to life.
+          </p>
+          <p>
+            We are committed to delivering a secure service with an intuitive
+            and user-friendly interface.
+          </p>
         </div>
+
+        {/* Left index comments */}
+
+      </div>
     </div>
-  )
+  );
 }
