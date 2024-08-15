e99370356a65e0dbcb1fa27fef06501c203dfab8
Tracy-miranja <barazatracy44@gmail.com>
2024-08-15 10:52:32 +0300

    Add team members

--- src/app/Components/Team.jsx
+++ src/app/Components/Team.jsx
@@ -10,13 +10,28 @@ function Team() {
         <p className="font-poppins font-sans">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
-        <button className="font-poppins font-sans bg-blue-900 mt-3 p-2 rounded-md text-white hover:bg-inherit hover:text-black hover:outline-1 hover:shadow-md">
+        <button className="font-poppins font-sans bg-blue-900 shadow-lg mt-3 p-2 rounded-md text-white hover:bg-inherit hover:text-black hover:outline-1 hover:shadow-md">
           About Team Members{" "}
         </button>
       </div>
-      <div className="w-[18%] h-[38vh] shadow-lg"></div>
-      <div className="w-[18%] h-[38vh] shadow-lg"></div>
-      <div className="w-[18%] h-[38vh] shadow-lg"></div>
+      <div className="w-[18%] h-[38vh] shadow-xl align-text-bottom">
+        <div className="w-full h-[30vh]"></div>
+        <div className="mb-0 flex justify-center">
+          <h4>Tracy Miranja</h4>
+        </div>
+      </div>
+      <div className="w-[18%] h-[38vh] shadow-lg">
+        <div className="w-full h-[30vh]"></div>
+        <div className="flex justify-center">
+          <h4>Tchasinga Asiwaju</h4>
+        </div>
+      </div>
+      <div className="w-[18%] h-[38vh] shadow-lg">
+        <div className="w-full h-[30vh]"></div>
+        <div className="flex justify-center">
+          <h4>Oscar winrar</h4>
+        </div>
+      </div>
     </div>
   );
 }
