5bf166e743ee6f3d7d217c4c6e919ab3569af433
Tracy-miranja <barazatracy44@gmail.com>
2024-08-19 15:00:47 +0300

    Correct error

--- src/app/Components/About.jsx
+++ src/app/Components/About.jsx
@@ -3,7 +3,7 @@ import "./Ui/about.css";
 
 function About() {
   return (
-    <div className="bg-black h-fit w-full">
+    <div className="bg-black h-fit w-[100%]">
       <div className="about-section h-[30vh] w-full relative bg-fixed">
         <div className="absolute inset-0 bg-sky-800 opacity-20"></div>
         <div className="relative flex justify-center items-center h-full w-full">
@@ -13,13 +13,13 @@ function About() {
         </div>
       </div>
       {/* second section */}
-      <div className="second flex bg-white h-fit sm:h-[30vh] w-full gap-5 justify-center flex-col items-center sm:flex-row lg:h-fit">
+      <div className="second flex bg-white h-fit md:h-[30vh] w-[100%] gap-5 justify-center flex-col items-center md:flex-row lg:h-fit">
         <div className="w-[90%] h-fit mt-4 md:w-[25%]">
           <h3 className="text-2xl text-center text-slate-900 font-poppins font-sans font-bold sm:text-blue-600">
             Where innovation meets excellence in the digital realm
           </h3>
         </div>
-        <div className=" w-[90%] p-2 md:w-[30%] md:h-fit">
+        <div className=" w-[90%] p-2 md:w-[30%] md:h-fit text-center md:text-start">
           <p className="font-poppins font-sans">
             <span className="text-blue-950 font-poppins font-sans font-bold text-2xl text-medium">
               W
@@ -34,7 +34,7 @@ function About() {
         </div>
         <div className="w-[90%] p-2 md:w-[30%] md:h-fit">
           {" "}
-          <p className="font-poppins font-sans">
+          <p className="font-poppins font-sans text-center md:text-start">
             strategic digital marketing and branding, ensuring you stand out in
             a crowded market. Plus, our professional training programs equip
             your team with the skills needed to thrive in today’s tech-driven
