b29772b86bd3ea40f35087c320397b51cd78b7da
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-26 15:14:16 +0200

    feat: Update page with new content and achievement message
    The page has been updated with new content, including a message thanking users for their support and mentioning that an achievement will be added soon. This change improves the user experience and provides a more engaging page for visitors.

--- src/app/(rooms)/design/page.jsx
+++ src/app/(rooms)/design/page.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function design() {
   return (
     <div>
-        <h1>Design</h1>
+       <div className="flex-col text-slate-700 font-bold text-3xl">
+       <h1>This woks achivement will added soon</h1>
+       <h1>Thanks you for stay around</h1>
+       </div>
     </div>
   )
 }
