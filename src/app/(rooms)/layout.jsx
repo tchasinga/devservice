5d5f4fc7bee9212228bf0b91f424684e012d80c0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-26 09:07:31 +0200

    chore: Add navigation links to layout component
    This commit adds navigation links to the layout component for the rooms page. The links include "Design", "Mobile", "Photo", "Webaplication", and "Website". These links will provide easy access to different sections of the website.
    
    Fixes #789

---
+++ src/app/(rooms)/layout.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinksnameRoute = [
+  { name: "Design", href: "/design" },
+  { name: "Mobile", href: "/mobile" },
+  { name: "Photo", href: "/photo" },
+  { name: "Webaplication", href: "/webaplication" },
+  { name: "Website", href: "/website" },
+];
