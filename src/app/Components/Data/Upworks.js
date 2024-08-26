89705754738897c88b5d68d08c3fd7b73bdb010c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-26 10:03:03 +0200

    feat: Add images to ProjectManager array

---
+++ src/app/Components/Data/Upworks.js
@@ -0,0 +1,14 @@
+
+import Singlepages from "../Balance/AmIdisplayResponsive.png";
+import Fitnessroom from "../Balance/crops.png";
+import workoursmanagement from "../Balance/DesignFive.png";
+import Reactnative from "../Balance/LearningNative.png";
+import Magazineshopping from "../Balance/magazineStuff.png";
+
+import projectSix from "../Balance/newScreenget.png";
+import projectSeven from "../Balance/Screenshot.png";
+import projectEight from "../Balance/ScreenshotStuff.png";
+import projectNine from "../Balance/TchatAppKozeri.png";
+
+ export const  ProjectManager = [
+]
