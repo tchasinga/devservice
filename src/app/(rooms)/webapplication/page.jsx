930226e41e1057f6f6785fea51835ea87f56b255
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-26 09:24:48 +0200

    feat: Add web application and website pages
    This commit adds the web application and website pages to the project. The web application page displays a heading "Web application" and the website page displays a heading "Website". These pages provide the foundation for the respective sections of the website.
    
    Fixes #456

---
+++ src/app/(rooms)/webapplication/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function webapplication() {
+  return (
+    <div>
+      <h1>Web application</h1>
+    </div>
+  )
+}
---
+++ src/app/(rooms)/website/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function website() {
+  return (
+    <div>
+        <h1>Website</h1>
+    </div>
+  )
+}
