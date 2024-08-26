9e2ff90e6a17171fc4b5419fd0395592a981bf59
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-26 10:18:36 +0200

    feat: Update image paths in ProjectManager array

--- src/app/Components/Data/Upworks.js
+++ src/app/Components/Data/Upworks.js
@@ -5,10 +5,99 @@ import workoursmanagement from "../Balance/DesignFive.png";
 import Reactnative from "../Balance/LearningNative.png";
 import Magazineshopping from "../Balance/magazineStuff.png";
 
-import projectSix from "../Balance/newScreenget.png";
-import projectSeven from "../Balance/Screenshot.png";
-import projectEight from "../Balance/ScreenshotStuff.png";
-import projectNine from "../Balance/TchatAppKozeri.png";
+import BlogsSharing from "../Balance/newScreenget.png";
+import LandEstate from "../Balance/Screenshot.png";
+import Stuffseller from "../Balance/ScreenshotStuff.png";
+import MessageApp from "../Balance/TchatAppKozeri.png";
 
  export const  ProjectManager = [
+    {
+        id: 1,
+        title: "Stuff-seller",
+        image: Stuffseller,
+        details:
+          "Stuff is a website that help you to get close of your own data or information security, and it can used as to-do list management",
+        techStack: "Mui, TailwindCss, Reactjs, Oop",
+        live: "https://stuff-seller.onrender.com/",
+        source: "https://github.com/tchasinga/e-comerce-site",
+      },
+      {
+        id: 2,
+        title: "Fitness room",
+        image: Fitnessroom,
+        details:
+          "Whether it's a small nook or an entire room, homeowners are starting to make home gyms a priority. Engaging in a home fitness routine doesn't mean you need expensive workout equipment; there are several home gym designs and home gym ideas that simply require some creativity, a little bit of ...",
+        techStack: "NextJs, TailwindCss , Anime Css , Swipper",
+        live: "https://fitness-rooms.netlify.app/",
+        source: "https://github.com/tchasinga/gym-rooms",
+      },
+      {
+        id: 3,
+        title: "React-native",
+        image: Reactnative,
+        details: "React Native combines the best parts of native development with React, a best-in-class JavaScript library for building user interfaces. Use a little—or a lot. You can use React Native today in your existing Android and iOS projects or you can create a whole new app from scratch.",
+        techStack: "Nextjs, TailwindCss, Framer-motions, animejs",
+        live: "https://native-present.netlify.app",
+        source: "https://github.com/tchasinga/native-present"
+      },
+      {
+        id: 4,
+        title: "Single-pages",
+        image: Singlepages,
+        details: "building server-rendered applications. While it&apos;s often used for server-side rendering, it can also be used to create single-page applications (SPAs) with client-side rendering. Here&apos;s a brief overview of how to create a single-page application using Next.js",
+        techStack: "Nextjs, Apis, TailwindCss, Framer-motions,",
+        live: "https://masingle-pages.netlify.app",
+        source: "https://github.com/tchasinga/single-page"
+      },
+      {
+        id: 5,
+        title: "Blogs sharing",
+        image: BlogsSharing,
+        details:
+          "Blogs ideas sharing involves the creation and dissemination of content that inspires, informs, or entertains an audience. Content creators share thoughts, experiences, and insights on diverse topic…",
+        techStack:
+          "Reactjs, MongoDB, Nodejs, Express, Firebase, tailwind Css, Material Ui",
+        live: "https://blogs-sharing-ideas.onrender.com",
+        source: "https://github.com/tchasinga/sharing-ideas-blogs",
+      },
+
+      {
+        id: 6,
+        title: "LandEstate",
+        image: LandEstate,
+        details:
+          "A Land Estate Typically Refers To A Large Area Of Land That Is Planned And Developed For Residential Or Commercial Purposes. These Estates Are Designed To Accommodate A Community, Providing Space For Housing, Amenities, And Infrastructure. The Purpose Of A Land Estate Is Multifaceted, Encompassing Both Economic And Social Objectives.",
+        techStack: "Reactjs, MongoDB, Nodejs, Express, Firebase, tailwind Css",
+        live: "https://landind-estate-service.onrender.com/",
+        source: "https://github.com/tchasinga/Real-Estate-house-Marketplace",
+      },
+      {
+        id: 7,
+        title: "workours-management",
+        image: workoursmanagement,
+        details:
+          "System Manager is responsible for the management of the system. It is also responsible for the maintenance of the system, uisng MERN stack",
+        techStack: "React, Tailwind css, MongoDB, Express, Nodejs",
+        live: "https://workours-management.onrender.com/",
+        source: "https://github.com/tchasinga/system-manager",
+      },
+      {
+        id: 8,
+        title: "Message App",
+        image: MessageApp,
+        details : "It seems like you might be referring to a message chatting application or functionality. Could you please provide more details on what you need help with? For instance, are you looking to implement…",
+        techStack : "Reactjs, Express, Nodejs, TailwindCss, Framer-motion, Sokect.io, Mongodb",
+        live : "https://messagge-tchatting.onrender.com",
+        source : "https://github.com/tchasinga/messagge-tchatting"
+       }, 
+       {
+        id: 9,
+        title: "Magazine shopping",
+        image: Magazineshopping,
+        details: "Magazine shopping refers to the practice of purchasing goods or services directly from advertisements or featured products found in magazines. Consumers may be influenced by the recommendations, re…",
+        techStack: "Nextjs, TailwindCss, Material Ui, Oop, Framer-motions",
+        live: "https://magazine-shopping.onrender.com/",
+        source: "https://github.com/tchasinga/magazine-shopping",
+      },
+  
 ]
