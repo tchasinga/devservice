bce033e71cab91a76e4ec4e5eb7ed11a38cee167
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-30 15:42:31 +0200

    chore: Refactor Heros component to improve background color change function

--- src/app/Components/Ui/Heros.jsx
+++ src/app/Components/Ui/Heros.jsx
@@ -21,7 +21,7 @@ export default function Heros() {
   const [bgColor, setBgColor] = useState(getRandomColor());
 
   useEffect(() => {
-    // Function to change the background color every 3 seconds
+    // Function to change the background 
     const interval = setInterval(() => {
       setBgColor(getRandomColor());
     }, 3000); // Change color every 3 seconds
